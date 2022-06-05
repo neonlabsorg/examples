fd15c641127c819090ec4d547f2c875c20dccbf6
Ivan Loboda <lobodatell@yandex.ru>
2022-06-05 22:17:04 +0300

    edit config

--- simple-erc20-hardhat/hardhat.config.js
+++ simple-erc20-hardhat/hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomiclabs/hardhat-waffle");
 
 const proxy_url = 'https://proxy.devnet.neonlabs.org/solana';
 const network_id = 245022926;
-const deployerPrivateKey = '0x42679bb84732ca108204abdd4841a716ba43593cba16a61f3289c0842e2f5e42';
+const deployerPrivateKey = 'PLACE YOUR PRIVATE KEY HERE';
 
 module.exports = {
   solidity: "0.8.4",
@@ -13,9 +13,6 @@ module.exports = {
       accounts: [deployerPrivateKey],
       network_id: network_id,
       chainId: network_id,
-      gas: 3000000,
-      gasPrice: 445297322800,
-      blockGasLimit: 10000000,
       allowUnlimitedContractSize: false,
       timeout: 1000000,
       isFork: true
