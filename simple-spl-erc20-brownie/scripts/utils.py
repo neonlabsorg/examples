8732b990768c74007766e24eee515f892437ed28
Eugeny Kurkovich <ek@neonlabs.org>
2022-11-14 16:53:24 +0300

    NDEV-809: Added deploy script

--- simple-spl-erc20-brownie/scripts/utils.py
+++ simple-spl-erc20-brownie/scripts/utils.py
@@ -1,63 +1,15 @@
-# coding: utf-8
-import os
-import random
-import string
-import typing as tp
-
+import time
 import requests
-from brownie import ERC20ForSplFactory
-from brownie import network
-
-try:
-    FAUCET_URL = os.environ["FAUCET_URL"]
-except KeyError as e:
-    e.args = (
-        "Please create .env file and paste `export FAUCET_URL=your_faucet_url` to it",
-    )
-    raise
 
 
 class Faucet:
-    def __init__(self, faucet_url: str, session: tp.Optional[tp.Any] = None) -> None:
+    def __init__(self, faucet_url: str) -> None:
         self._url = faucet_url
-        self._session = session or requests.Session()
+        self._session = requests.Session()
 
-    def request_neon(
-        self, account: network.account.LocalAccount, amount: int = 1000
-    ) -> None:
+    def request_neon(self, account: str, amount: int = 1000) -> None:
         response = self._session.post(
-            self._url, json={"amount": amount, "wallet": account.address}
+            self._url, json={"amount": amount, "wallet": account}
         )
         response.raise_for_status()
-
-
-class SplERC20Brownie:
-    """Implements base SPL ERC20 pipeline"""
-
-    def __init__(
-        self, account: tp.Optional[network.account.LocalAccount] = None
-    ) -> None:
-        self._account = account or network.accounts.add()
-        self._faucet = Faucet(FAUCET_URL)
-        self._faucet.request_neon(self._account)
-        self._contract = self._deploy_from_factory(ERC20ForSplFactory)
-
-    def _deploy_from_factory(
-        self, contract_factory: network.contract.ContractContainer
-    ) -> network.contract.ProjectContract:
-        """Deploy contract from factory"""
-        contract = contract_factory.deploy({"from": self._account.address})
-        symbol = "".join(random.sample(string.ascii_uppercase, 3))
-        instruction_tx = contract.createErc20ForSplMintable(
-            "SPL ERC20 test", symbol, 18, self._account.address
-        )
-        return contract
-
-    #
-    # account = get_account()
-    # erc_contract = ERC20.deploy({"from": account})
-    # print(f"erc contract is: {erc_contract}")
-    # amount = 100 * 10 ** 9
-    # mint_tx = erc_contract.mint(amount)
-    # print(f"minting...{amount}")
-    # mint_tx.wait(1)
+        time.sleep(3)
---
+++ simple-spl-erc20-brownie/tests/test_spl_erc2.py
@@ -0,0 +1,71 @@
+# coding: utf-8
+import os
+import random
+import string
+
+import pytest
+from brownie import ERC20ForSplMintable as contract_factory
+from brownie import network
+
+from ..utils import Faucet
+
+FAUCET_URL = os.environ["FAUCET_URL"]
+
+
+@pytest.fixture
+def faucet() -> Faucet:
+    return Faucet(FAUCET_URL)
+
+
+@pytest.fixture(scope="session")
+def sender(faucet: Faucet) -> network.account.LocalAccount:
+    """Create tokens sender account"""
+    account = network.accounts.add()
+    print(f"request neon for {account.address[:10]}")
+    amount = pow(10, 4)
+    faucet.request_neon(account.address, amount)
+    assert account.balance() == amount
+    return account
+
+
+@pytest.fixture(scope="session")
+def recipient() -> network.account.LocalAccount:
+    """Create recipient account"""
+    return network.accounts.add()
+
+
+@pytest.fixture(scope="session")
+def contract(
+    sender: network.account.LocalAccount,
+) -> network.contract.ProjectContract:
+    """Deploy contract from factory"""
+    if not isinstance(sender, str):
+        account = sender.address
+    symbol = "".join(random.sample(string.ascii_uppercase, 3))
+    contract_name = f"spl erc20 {symbol}"
+    print(f"deploy `ERC20` contract")
+    contract = contract_factory.deploy(
+        contract_name, symbol, 18, account, {"from": account}
+    )
+    print(f"contract deployed `{contract.address}`")
+    amount = pow(10, 4)
+    print(f"mint amount `{amount}`")
+    contract.mint(account, amount, {"from": account})
+    assert contract.balanceOf(account) == amount
+    return contract
+
+
+def test_token_transfer(
+    contract: network.contract.ProjectContract,
+    sender: network.account.LocalAccount,
+    recipient: network.account.LocalAccount,
+) -> None:
+    """Check tokens transfer"""
+    amout = 100
+    neon_balance = sender.balance()
+    erc20_balance = contract.balanceOf(sender.address)
+    print(f"transfer tokens from `{sender.address[:10]}` to `{recipient.address[:10]}`")
+    contract.transfer(recipient.address, amout, {"from": sender.address})
+    assert contract.balanceOf(recipient.address) == amout
+    assert contract.balanceOf(sender.address) == erc20_balance - amout
+    assert sender.balance() < neon_balance
