1ad51eb5d70b26b0d0860677a6cebb9f09c63c6c
sukanyaparashar <sukanyaparashar111@gmail.com>
2023-08-11 18:45:31 -0700

    Add simple erc721 example

---
+++ simple-erc721-foundry/test/MyFirstNFT.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.21;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {MyFirstNFT} from "../src/MyFirstNFT.sol";
+
+contract MyFirstNFTTest is Test {
+    
+}
