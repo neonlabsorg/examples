6ac795ce7dabf5b77afb5006cf16fe1747436833
sukanyaparashar <sukanyaparashar111@gmail.com>
2023-08-15 22:35:37 -0700

    Add tests and README

--- simple-erc721-foundry/test/MyFirstNFT.t.sol
+++ simple-erc721-foundry/test/MyFirstNFT.t.sol
@@ -5,5 +5,24 @@ import {Test, console2} from "forge-std/Test.sol";
 import {MyFirstNFT} from "../src/MyFirstNFT.sol";
 
 contract MyFirstNFTTest is Test {
-    
+    MyFirstNFT token;
+
+    address alice = vm.addr(0x1);
+
+    function setUp() public {
+        token = new MyFirstNFT();
+    }
+
+    function testName() external {
+        assertEq("MyFirstNFT", token.name());
+    }
+
+    function testSymbol() external {
+        assertEq("FNFT", token.symbol());
+    }
+
+    function testMint() public {
+        token.safeMint(alice, "https://apricot-official-sailfish-923.mypinata.cloud/ipfs/QmRsP8VDyPZ54xC3FXJqAtZpBTW8CXQmHUsvJEbc5q7vKG");
+        assertEq("https://apricot-official-sailfish-923.mypinata.cloud/ipfs/QmRsP8VDyPZ54xC3FXJqAtZpBTW8CXQmHUsvJEbc5q7vKG", token.tokenURI(0));
+    }
 }
